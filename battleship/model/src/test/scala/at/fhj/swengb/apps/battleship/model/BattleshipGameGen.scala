7dc35ea0879373301a24255e6cd5df1a4ccd6b62
flamingo25 <eliska.krnavkova@edu.fh-joanneum.at>
2017-12-28 13:24:44 +0100

    BatttleShippApp small additional changes

--- battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/model/BattleshipGameGen.scala
+++ battleship/model/src/test/scala/at/fhj/swengb/apps/battleship/model/BattleshipGameGen.scala
@@ -1,36 +1,11 @@
 package at.fhj.swengb.apps.battleship.model
 
-import org.scalacheck.Gen
-
 
 /**
   * Implement in the same manner like MazeGen from the lab, adapt it to requirements of BattleShip
   */
 object BattleshipGameGen {
 
-  val maxWidth: Int = 10
-  val maxHeight: Int = 10
-  val fleetConfigSeq: Seq[FleetConfig] = Seq(FleetConfig.OneShip, FleetConfig.TwoShips, FleetConfig.Standard)
+  ???
 
-
-  //Generate a random fleet for Battleship
-  val fleetGen: Gen[Fleet] = for {
-    x <- Gen.chooseNum[Int](0, fleetConfigSeq.size - 1)
-  } yield Fleet(fleetConfigSeq(x))
-
-  //Generate a random battlefield
-  val battlefieldGen: Gen[BattleField] = for {
-    width <- Gen.chooseNum[Int](1, maxWidth)
-    height <- Gen.chooseNum[Int](1, maxHeight)
-    fleet <- fleetGen
-  } yield BattleField(width, height, fleet)
-
-
-  val battleShipGameGen: Gen[BattleShipGame] = for {
-    battlefield <- battlefieldGen
-    //cellWidth <- cellGen
-    //cellHeigth <- cellGen
-  } yield {
-    BattleShipGame(battlefield, ( x => x.toDouble), ( x => x.toDouble), (x => println(x)))
-  }
 }
